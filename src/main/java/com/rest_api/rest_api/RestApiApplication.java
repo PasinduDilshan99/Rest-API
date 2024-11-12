19df2141d95ad0c115ba2eb2ad4f25f66d447973
Pasindu Dilshan Dimbulana <pasindudi@mobitel.lk>
2024-11-12 13:50:35 +0530

    update

--- src/main/java/com/rest_api/rest_api/RestApiApplication.java
+++ src/main/java/com/rest_api/rest_api/RestApiApplication.java
@@ -3,11 +3,14 @@ package com.rest_api.rest_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class RestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("args : " + Arrays.toString(args));
+        SpringApplication.run(RestApiApplication.class, args);
+    }
 
 }
--- src/main/java/com/rest_api/rest_api/model/User.java
+++ src/main/java/com/rest_api/rest_api/model/User.java
@@ -3,10 +3,8 @@ package com.rest_api.rest_api.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
